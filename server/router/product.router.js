66945b50a41fd07f1ad36c8ba6da77fb535c41c1
Leah Shitrit <leahsh1093@gmail.com>
2025-09-18 13:29:42 +0300

    prod

--- server/router/product.router.js
+++ server/router/product.router.js
@@ -6,8 +6,7 @@ import { authMiddleware } from "../middlewares/auth.js";
 import { ProductController } from "../controllers/product.controller.js";
 const productController = new ProductController();
 const productRouter = express.Router();
-productRouter.get('/', productController.getAllProducts);
-productRouter.get('/:slug', productController.getProductsBySlug);
+
 
 productRouter.get("/new", productController.getNewProducts);
 productRouter.get('/', productController.getAllProducts);
