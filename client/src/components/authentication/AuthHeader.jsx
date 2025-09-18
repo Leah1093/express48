f46417d09b69e0f12168857f1b3501582485a135
Leah Shitrit <leahsh1093@gmail.com>
2025-09-18 12:45:23 +0300

    שיפור

--- client/src/components/authentication/AuthHeader.jsx
+++ client/src/components/authentication/AuthHeader.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export default function AuthHeader() {
     const isMobile = useSelector((state) => state.ui.isMobile);
@@ -10,7 +11,13 @@ export default function AuthHeader() {
         return (
             <div className="sticky top-0 z-50 w-full flex items-center justify-between p-5 bg-[#FFF7F2] shadow-sm">
                 <div className="absolute left-1/2 transform -translate-x-1/2">
-                    <img src="/MobileLogo.png" alt="MobileLogo" className="h-14" />
+                    <Link to="/">
+                        <img
+                            src="/MobileLogo.png"
+                            alt="MobileLogo"
+                            className="h-14 hover:cursor-pointer"
+                        />
+                    </Link>
                 </div>
                 <button
                     type="button"
@@ -26,7 +33,13 @@ export default function AuthHeader() {
     return (
         <div className="sticky top-0 z-50 mt-8 w-full flex items-center p-6 md:px-[10%] bg-white  ">
             <div className="absolute left-1/2 transform -translate-x-1/2">
-                <img src="/MobileLogo.png" alt="MobileLogo" className="h-16" />
+                <Link to="/">
+                    <img
+                        src="/MobileLogo.png"
+                        alt="MobileLogo"
+                        className="h-14 hover:cursor-pointer"
+                    />
+                </Link>
             </div>
 
             <button
