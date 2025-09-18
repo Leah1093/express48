44ad90a2d119f70ff6a5bb2dd3a337da011c496f
Kibelevich <m0556780320@gmail.com>
2025-09-18 13:06:18 +0300

    start tests

--- server/service/product.service.js
+++ server/service/product.service.js
@@ -36,6 +36,24 @@ class ProductService {
             throw err;
         }
     }
+
+    getAllProductsService = async () => {
+        try {
+            const products = await Product.find({}).populate('storeId');
+            return products;
+        } catch (err) {
+            throw new Error('Error fetching products');
+        }
+    }
+
+    getProductBySlugService = async (slug) => {
+        try {
+            const product = await Product.findOne({ slug }).populate('storeId');
+            return product;
+        } catch (err) {
+            throw new Error('Error fetching product by slug');
+        }
+    }
 }
 
 export const productService = new ProductService();
