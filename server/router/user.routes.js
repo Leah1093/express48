42bc69757965e941704fb552f6cf7395e386caa6
Leah Shitrit <leahsh1093@gmail.com>
2025-12-11 13:07:48 +0200

    תיקון קוד

--- server/router/user.routes.js
+++ server/router/user.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { UserController } from "../controllers/user.controller.js";
 
-import {
-  profileSchema,
-  passwordSchema,
-} from "../../client/src/components/validations/profileSchema.js";
-import { validate } from "../middlewares/validate.js";
+// import {
+//   profileSchema,
+//   passwordSchema,
+// } from "../../client/src/components/validations/profileSchema.js";
+// import { validate } from "../middlewares/validate.js";
 
 import { authMiddleware } from "../middlewares/auth.js";
 
